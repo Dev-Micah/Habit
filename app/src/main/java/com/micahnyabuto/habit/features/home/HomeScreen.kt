bc41e7a95564ae58cec7a7707028b7e290e72235
Micah Nyabuto <atembamicah@gmail.com>
2025-06-17 23:26:07 +0300

    Changed greeting fun to utils

--- app/src/main/java/com/micahnyabuto/habit/features/home/HomeScreen.kt
+++ app/src/main/java/com/micahnyabuto/habit/features/home/HomeScreen.kt
@@ -6,14 +6,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.micahnyabuto.habit.utils.Greeting
+import java.util.Calendar
 
 @Composable
 fun HomeScreen(){
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ){
-        Text(text = "Home Screen")
+Greeting()
+}
 
-    }
+@Composable
+fun HomeScreenContent(){
+
 }
