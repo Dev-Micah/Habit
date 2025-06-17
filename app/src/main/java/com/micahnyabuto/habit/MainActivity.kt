faa249a2bc3c4c1a843f0ea158ef3833593bb11f
Micah Nyabuto <atembamicah@gmail.com>
2025-06-17 23:16:34 +0300

    Update

--- app/src/main/java/com/micahnyabuto/habit/MainActivity.kt
+++ app/src/main/java/com/micahnyabuto/habit/MainActivity.kt
@@ -5,8 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.rememberNavController
-import com.micahnyabuto.habit.features.authentication.signIn.SignInScreen
-import com.micahnyabuto.habit.navigation.AppNavHost
+import com.micahnyabuto.habit.core.navigation.AppNavHost
 import com.micahnyabuto.habit.ui.theme.HabitTheme
 
 class MainActivity : ComponentActivity() {
