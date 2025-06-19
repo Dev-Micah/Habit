ff8e8089bc2649dced49f49cdf283bbf67017cc0
Micah Nyabuto <atembamicah@gmail.com>
2025-06-20 00:46:22 +0300

    Update

--- app/src/main/java/com/micahnyabuto/habit/di/AppModule.kt
+++ app/src/main/java/com/micahnyabuto/habit/di/AppModule.kt
@@ -2,7 +2,6 @@ package com.micahnyabuto.habit.di
 
 import androidx.room.Room
 import com.micahnyabuto.habit.core.data.local.HabitDatabase
-import com.micahnyabuto.habit.core.data.repository.HabitRepository
 import com.micahnyabuto.habit.core.data.repository.HabitRepositoryImpl
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
