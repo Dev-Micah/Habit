faa9812989bf5b0b3a45551ad18a97da0451a11a
Micah Nyabuto <atembamicah@gmail.com>
2025-06-22 14:26:43 +0300

    Updated RepositoryImpl

--- app/src/main/java/com/micahnyabuto/habit/core/data/repository/HabitRepository.kt
+++ app/src/main/java/com/micahnyabuto/habit/core/data/repository/HabitRepository.kt
@@ -5,28 +5,28 @@ import com.micahnyabuto.habit.core.data.local.HabitEntity
 import kotlinx.coroutines.flow.Flow
 
 interface HabitRepository {
-    val allHabits: Flow<List<HabitEntity>>
+    fun allHabits(): Flow<List<HabitEntity>>
 
     suspend fun insertHabit(habit: HabitEntity)
     suspend fun updateHabit(habit: HabitEntity)
     suspend fun deleteHabit(habit: HabitEntity)
 }
 
-class HabitRepositoryImpl(private val habitDao: HabitDao) {
+class HabitRepositoryImpl(private val habitDao: HabitDao): HabitRepository {
 
-    val allHabits: Flow<List<HabitEntity>> = habitDao.getAllHabits()
+    override fun allHabits(): Flow<List<HabitEntity>> = habitDao.allHabits()
 
 
-    suspend fun insertHabit(habit: HabitEntity) {
+    override suspend fun insertHabit(habit: HabitEntity) {
         habitDao.insertHabit(habit)
     }
 
 
-    suspend fun updateHabit(habit: HabitEntity) {
+    override suspend fun updateHabit(habit: HabitEntity) {
         habitDao.updateHabit(habit)
     }
 
-    suspend fun deleteHabit(habit: HabitEntity) {
+    override suspend fun deleteHabit(habit: HabitEntity) {
         habitDao.deleteHabit(habit)
     }
 }
