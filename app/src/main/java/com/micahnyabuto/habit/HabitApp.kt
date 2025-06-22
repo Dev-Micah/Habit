a1377fe02cb8f186f004afd83d90185be46cf089
Micah Nyabuto <atembamicah@gmail.com>
2025-06-22 14:25:08 +0300

    Updated Application class

--- app/src/main/java/com/micahnyabuto/habit/HabitApp.kt
+++ app/src/main/java/com/micahnyabuto/habit/HabitApp.kt
@@ -1,9 +1,16 @@
 package com.micahnyabuto.habit
 
 import android.app.Application
+import com.micahnyabuto.habit.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class HabitApp: Application(){
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            androidContext(this@HabitApp)
+            modules(appModule)
+        }
     }
 }
