d38ac70b3c86d5f6efbf2aeec941c70ce2b67f0a
Micah Nyabuto <atembamicah@gmail.com>
2025-07-03 14:01:17 +0300

    Added Android Logger in Application class

--- app/src/main/java/com/micahnyabuto/habit/HabitApp.kt
+++ app/src/main/java/com/micahnyabuto/habit/HabitApp.kt
@@ -3,12 +3,15 @@ package com.micahnyabuto.habit
 import android.app.Application
 import com.micahnyabuto.habit.di.appModule
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import java.util.logging.Level
 
 class HabitApp: Application(){
     override fun onCreate() {
         super.onCreate()
         startKoin {
+            androidLogger(org.koin.core.logger.Level.DEBUG)
             androidContext(this@HabitApp)
             modules(appModule)
         }
